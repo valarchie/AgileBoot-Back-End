d665c956b958214d26d91db21f66cc5fd65876bb
valarchie <343928303@qq.com>
2023-01-17 18:10:40 +0800

    refactor: 修正Jackson的自定义deserializer.覆盖yml中的配置问题。

--- agileboot-infrastructure/src/main/java/com/agileboot/infrastructure/security/xss/JsonXssConfiguration.java
+++ agileboot-infrastructure/src/main/java/com/agileboot/infrastructure/security/xss/JsonXssConfiguration.java
@@ -1,6 +1,6 @@
 package com.agileboot.infrastructure.security.xss;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
@@ -8,14 +8,19 @@ import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
  * @author valarchie
  */
 @Configuration
-public class JsonXssConfiguration {
+public class JsonXssConfiguration implements Jackson2ObjectMapperBuilderCustomizer{
 
-    @Bean
-    Jackson2ObjectMapperBuilder objectMapperBuilder() {
-        Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
-        builder.deserializers(new JsonHtmlXssTrimSerializer());
-        return builder;
+//    这种配置方式会覆盖 yml中的jackson配置， 使用下面的customize配置则不会
+//    @Bean
+//    Jackson2ObjectMapperBuilder objectMapperBuilder() {
+//        Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
+//        builder.deserializers(new JsonHtmlXssTrimSerializer());
+//        return builder;
+//    }
+
+    @Override
+    public void customize(Jackson2ObjectMapperBuilder jacksonObjectMapperBuilder) {
+        jacksonObjectMapperBuilder.deserializers(new JsonHtmlXssTrimSerializer());
     }
-
 
 }
