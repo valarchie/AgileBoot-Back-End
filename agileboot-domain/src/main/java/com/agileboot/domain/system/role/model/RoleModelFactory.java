3952d2077b3c5a27d1ba7c9876c001ffb271ddad
valarchie <343928303@qq.com>
2023-02-22 21:32:38 +0800

    fix: 修改角色菜单丢失的Bug

--- agileboot-domain/src/main/java/com/agileboot/domain/system/role/model/RoleModelFactory.java
+++ agileboot-domain/src/main/java/com/agileboot/domain/system/role/model/RoleModelFactory.java
@@ -1,10 +1,16 @@
 package com.agileboot.domain.system.role.model;
 
+import cn.hutool.core.convert.Convert;
+import cn.hutool.core.util.StrUtil;
 import com.agileboot.common.exception.ApiException;
 import com.agileboot.common.exception.error.ErrorCode;
 import com.agileboot.orm.system.entity.SysRoleEntity;
+import com.agileboot.orm.system.entity.SysRoleMenuEntity;
 import com.agileboot.orm.system.service.ISysRoleMenuService;
 import com.agileboot.orm.system.service.ISysRoleService;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -28,7 +34,20 @@ public class RoleModelFactory {
         if (byId == null) {
             throw new ApiException(ErrorCode.Business.OBJECT_NOT_FOUND, roleId, "角色");
         }
-        return new RoleModel(byId, roleService, roleMenuService);
+
+        LambdaQueryWrapper<SysRoleMenuEntity> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(SysRoleMenuEntity::getRoleId, roleId);
+        List<Long> menuIds = roleMenuService.list(queryWrapper).stream().map(SysRoleMenuEntity::getMenuId)
+            .collect(Collectors.toList());
+        List<Long> deptIds = StrUtil.split(byId.getDeptIdSet(), ",").stream()
+            .map(Convert::toLong).collect( Collectors.toList());
+
+        RoleModel roleModel = new RoleModel(byId, roleService, roleMenuService);
+
+        roleModel.setMenuIds(menuIds);
+        roleModel.setDeptIds(deptIds);
+
+        return roleModel;
     }
 
     public RoleModel create() {
