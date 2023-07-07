0b0edfa7a563fb077a8b0fd6942217f5cb41e76a
valarchie <343928303@qq.com>
2023-07-07 18:20:50 +0800

    docs: 添加关于securityConfig的注释

--- agileboot-infrastructure/src/main/java/com/agileboot/infrastructure/config/SecurityConfig.java
+++ agileboot-infrastructure/src/main/java/com/agileboot/infrastructure/config/SecurityConfig.java
@@ -137,6 +137,7 @@ public class SecurityConfig {
             // 过滤请求
             .authorizeRequests()
             // 对于登录login 注册register 验证码captchaImage 以及公共Api的请求允许匿名访问
+            // 注意： 当携带token请求以下这几个接口时 会返回403的错误
             .antMatchers("/login", "/register", "/getConfig", "/captchaImage", "/api/**").anonymous()
             .antMatchers(HttpMethod.GET, "/", "/*.html", "/**/*.html", "/**/*.css", "/**/*.js",
                 "/profile/**").permitAll()
